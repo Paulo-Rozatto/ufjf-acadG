5d5e6413f27c1a623437a41c83a1b90858581e65
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2022-02-13 03:25:48 -0300

    changed login structure

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-import { Button, TextField } from '@mui/material'
+import { Button, Grid, TextField } from '@mui/material'
 import { Navigate } from "react-router-dom";
 
 export default class Login extends React.Component {
@@ -37,17 +37,25 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                <h2>Login</h2>
-                <TextField id="logn" label="Login" variant="filled" required />
-                <br /><br />
-                <TextField id="password" label="Senha" variant="filled" required type="password" />
-                <br /><br />
-                <Button onClick={this.handleSubmit} variant="contained">Enviar</Button>
-                {
-                    this.state.navigate && <Navigate to={this.state.path} state={this.state.user}></Navigate>
-                }
-            </div>
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+
+                style={{height: '100vh', backgroundColor: '#cccccc'}}
+            >
+                <Grid xs={8} md={6} item style={{ backgroundColor: '#00aff0a0', padding: '1em'}} >
+                    <div style={{margin: 'auto', width: '80%'}}>
+                        <h2>Login</h2>
+                        <TextField id="logn" label="Login" variant="filled" required  fullWidth/>
+                        <br /><br />
+                        <TextField id="password" label="Senha" variant="filled" required fullWidth type="password" />
+                        <br /><br />
+                        <Button onClick={this.handleSubmit} variant="contained">Enviar</Button>
+                        {this.state.navigate && <Navigate to={this.state.path} state={this.state.user}></Navigate>}
+                    </div>
+                </Grid>
+            </Grid>
         );
     }
 }
